e0b0958d208a7a4bcc84dac44197ef772df0323e
leoherrerabcde <leo.herrera.bcde@gmail.com>
2019-08-06 22:23:03 -0400

    changes

--- main.cpp
+++ main.cpp
@@ -284,7 +284,7 @@ int main(int argc, char* argv[])
 
         if (/*bConnecting ==true || */bConnected == true)
         {
-            if (sckComPort.getState() == sckError)
+            if (sckComPort.getSocketState() == sckError)
             {
                 if (remotePort)
                 {
